b3fe6c65a93057f24eaa2a95f363414b80fdb8f2
BlaiseCJ <ezeblaise@gmail.com>
2022-05-23 19:45:44 +0100

    Create methods.py
    Methods show the extended commands functionalities with the usual input and the print functions.

---
+++ methods.py
@@ -0,0 +1,7 @@
+status = "He is married"
+print(status.replace("married","single"))
+print(status.find("is"))
+print(status)
+print("I say " + status.upper())
+print("if one is neither single nor married, one could be widowed or partnered")
+print("married" in status)
