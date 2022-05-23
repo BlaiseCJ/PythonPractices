b758f12e84a6e3a8f10f1ac9435b11e8af119990
BlaiseCJ <ezeblaise@gmail.com>
2022-05-23 20:25:49 +0100

    Create arithmetic.py
    This shows the basic arithmetic operators in python.
    
    I practiced them, using # to comment what they are for.

---
+++ arithmetic.py
@@ -0,0 +1,17 @@
+x = 10 == 5 + 5 #equal to
+print(x)
+
+y = 20 <= 7 #greater than or equal to
+print(y)
+
+p = 3 ** 3  #power of
+print(p)
+
+a = 10//3 #division to get integer
+print(a)
+
+b = 30 / 14 #arithmetic with float
+print(b)
+
+m = 3 != 5 # not equal to
+print(m)
