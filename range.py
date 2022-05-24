4973507e773b166206805e03c75ea85182a057d0
BlaiseCJ <ezeblaise@gmail.com>
2022-05-24 09:37:55 +0100

    Create range.py
    This basic range functions shows the presentation and sequence of numbers in a range.
    
    For loop is used here to represent sequence

---
+++ range.py
@@ -0,0 +1,13 @@
+# range functions
+
+numbers = range(5)   # here the range of the numbers in the bracket will read from 0 to 4
+for number in numbers:
+    print(number)
+
+numberss = range(4,12)  # with two variables in the bracket, the first begins while the second ends the sequence
+for mark in numberss:
+    print(mark)
+
+men = range(13,19,2) # With three numbers, the last shows the steps in the sequence.
+for quote in men:
+    print(quote)
