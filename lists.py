0537703bd06d83d7a740822e52f3eaaa93dd8c1f
BlaiseCJ <ezeblaise@gmail.com>
2022-05-24 05:54:00 +0100

    Create lists.py
    This list operation names items in a list.

---
+++ lists.py
@@ -0,0 +1,7 @@
+fruits = ["pawpaw","orange","mango","cashew"]
+print(fruits)        # this prints the entire list
+print(fruits[0])     #this prints the first item on the list
+print(fruits[-1])    #reads the last item on the list
+print(fruits[0:3])   # list the index from 0 - 3
+fruits[0] = "papaya" #changed the first item in the list
+print(fruits)        # this prints the list with the updated "papaya" instead of "pawpaw"
