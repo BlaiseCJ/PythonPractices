52245c17dc641d1a5ecada845dd2c2c071f65eee
BlaiseCJ <ezeblaise@gmail.com>
2022-05-24 07:37:29 +0100

    Create ForLoop.py
    For Loop is used to list items in the list separately.
    
    It is a lot simpler than using the while loop which is also shown here

---
+++ ForLoop.py
@@ -0,0 +1,12 @@
+words = [1,2,3,4,5,6]
+
+for item in words: #This helps to itemise lists in separate lines
+    print(item)
+
+#Another way to achieve this is by using the while loop
+
+i = 0
+while i <= len(words):
+           print(words[i])
+           i = i + 1
+
